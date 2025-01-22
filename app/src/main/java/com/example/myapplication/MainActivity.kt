51c2e10ac011484ec273c11b2067a469e749a0f3
Neel Doshi <neeldoshi147@gmail.com>
2025-01-22 08:29:21 +0530

    Bottom Navigation Navigates to CartScreen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,18 +5,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.myapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding?=null
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.homeScreen -> {
+                    navController.navigate(R.id.restaurantFragment)
+                    true
+                }
+                R.id.cartScreen -> {
+                    navController.navigate(R.id.cartFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
